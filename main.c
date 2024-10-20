af69d6e8a57d94b78e70d1385829f9807592b558
nerdycuber <nerdycuber@tutamail.com>
2024-10-20 13:31:21 -0400

    fixed duplicate issue

--- main.c
+++ main.c
@@ -1,10 +1,20 @@
-#include<time.h>
-#include<stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include "config_parsing.h"
 
 int main(int argc, char** argv){
-
+    
+    int* used = malloc(sizeof(int) * 1);
+    int x;
+    int used_size = 0;
+    int used_cap = 1;
+    bool unique = false;
+    if(used == NULL){
+        printf("failed to allocate memory");
+        exit(1);
+    }
     int chara_num;
     char** chara_output = malloc(1 * sizeof(char*));
     if(chara_output == NULL){
@@ -27,14 +37,40 @@ int main(int argc, char** argv){
 
     for(int i = 0; i < chara_num; i++){
          if(chara_output_size == chara_output_cap){
-            chara_output = realloc(chara_output, sizeof(char*) * chara_output_cap * 2);
+            chara_output = realloc(chara_output, (sizeof(char*) * chara_output_cap) * 2);
             if(chara_output == NULL){
                 printf("faild to reallocate memory");
                 exit(1);
             }
             chara_output_cap *= 2;
         }
-        chara_output[i] = chara_list[rand() % chara_size];
+        unique = false;
+        while(!unique){
+            x = rand() % chara_size;
+            unique = true;
+            for(int j = 0; j < used_size; j++){
+                if(x == used[j]){
+                    unique = false;
+                    break;
+                }
+            }
+        }
+
+    
+        if(used_size == used_cap){
+            used = realloc(used, used_cap * sizeof(int) * 2);
+            if(used == NULL){
+                printf("failed to reallocate pointer");
+                exit(1);
+            }
+        
+        used_cap *= 2;
+        }
+        used[used_size] = x;
+        used_size += 1;
+
+        
+        chara_output[i] = chara_list[x];
         chara_output_size += 1;
     }
 
